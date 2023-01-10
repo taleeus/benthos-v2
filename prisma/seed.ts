eef869dd52085f30f0d22af95034ecda3b3bb1af
Alessandro Tagliani <alessandro.tagliani@tuta.io>
2023-01-10 10:15:39 +0100

    chore(db): changed seeded users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,11 +9,7 @@ async function seedUsers() {
         password: "ba4f288e81acd9b1ca3388f3702660c435b6f854d2fd320e15e4ce2fb24afb83",
       },
       {
-        username: "press",
-        password: "e996c8274e2115784422ab4e65de11dba61d1a6fb6393bb3df6ed744e849b177",
-      },
-      {
-        username: "booking",
+        username: "user",
         password: "aed10f1dc43da9d111d0644cb623360f8c4c7820c01ff525fa9c132f81b5c12a",
       },
     ],
