9c0a77ccab035223cf94387c6363a0c7fcfb6709
Amalia Cicala <cicala.amalia@gmail.com>
2023-01-27 15:36:13 +0100

    feat: create dissonance section (fixes #30)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
 import Section from "../components/Section.vue";
 import MusicBox from "../components/MusicBox.vue";
 import VideosSection from "../components/sections/VideosSection.vue";
+import { useScreenStore } from "../stores/useScreen";
+import { storeToRefs } from "pinia";
+
+const { isMobile } = storeToRefs(useScreenStore());
 
 const videoIds = ["hzknSmWxw2I", "TQuuQxOuI_k", "iXOm0oeMGZc", "1tVxbYR59Gc"];
 </script>
@@ -11,6 +15,38 @@ const videoIds = ["hzknSmWxw2I", "TQuuQxOuI_k", "iXOm0oeMGZc", "1tVxbYR59Gc"];
     <div
       class="h-[35vh] w-full bg-debris-bside-gradient bg-cover bg-top lg:h-[48rem]"
     ></div>
+    <Section
+      title="Dissonance Festival 2023"
+      class="flex flex-col items-center justify-center bg-dissonance bg-cover bg-center text-white"
+    >
+      <a
+        class="flex justify-center drop-shadow-lg lg:w-4/5"
+        href="https://linktr.ee/dissonancefestival"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          v-show="!isMobile"
+          src="/assets/backgrounds/dissonance-flyer-desktop.avif"
+          alt="Dissonance Festival 2023 Flyer"
+        />
+        <img
+          v-show="isMobile"
+          src="/assets/backgrounds/dissonance-flyer-mobile.avif"
+          alt="Dissonance Festival 2023 Flyer"
+        />
+      </a>
+      <div class="mt-6 flex gap-10 lg:mt-14">
+        <a
+          href="https://linktr.ee/dissonancefestival"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="rounded-lg bg-white py-4 px-10 font-medium uppercase text-black hover:bg-lightGrey"
+          >Tickets</a
+        >
+      </div></Section
+    >
+
     <Section title="Music" class="bg-black pb-16 text-white">
       <div class="justify-around md:flex lg:flex-row">
         <MusicBox
@@ -27,12 +63,12 @@ const videoIds = ["hzknSmWxw2I", "TQuuQxOuI_k", "iXOm0oeMGZc", "1tVxbYR59Gc"];
     </Section>
     <Section
       title="Tour Dates"
-      class="flex-col bg-black bg-tour bg-cover bg-center text-white lg:bg-center"
+      class="flex-col bg-tour bg-cover bg-center text-white lg:bg-center"
     >
       There are no tours announced at the moment.
       <div class="my-10">
         <a
-          href="https://www.songkick.com/artists/5048318-benthos"
+          href="https://www.bandsintown.com/a/1419641-benthos?came_from=257&utm_medium=web&utm_source=home&utm_campaign=search_bar"
           target="_blank"
           rel="noopener noreferrer"
           class="rounded-lg bg-white py-4 px-6 font-medium uppercase text-black hover:bg-lightGrey"
