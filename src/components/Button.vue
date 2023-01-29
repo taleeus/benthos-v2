986e65e0c0ffda0cb1184079ac8dfa95fcffa236
Amalia Cicala <cicala.amalia@gmail.com>
2023-01-29 16:22:40 +0100

    feat: edit epk bio to add dissonance announcement; edit background in homepage's music section (fixes #38)

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <button
-    class="my-2 mx-auto w-fit rounded-md bg-white p-2 py-3 px-5 text-base font-medium uppercase text-black transition-all duration-400 hover:bg-cream hover:opacity-90 focus:outline-none lg:my-4"
+    class="my-2 mx-auto w-fit rounded-md bg-white p-2 py-3 px-5 text-base font-medium uppercase text-black transition-all duration-400 hover:opacity-90 focus:outline-none lg:my-4"
     @click="clickHandler"
   >
     <slot></slot>
--- src/views/Epk.vue
+++ src/views/Epk.vue
@@ -35,41 +35,55 @@ const galleryUris = [
       <Section title="Bio" class="bg-black text-egg">
         <div class="text-justify text-xs lg:text-base">
           <p>
-            A curious, experimental, pure rock unit based out of Milan, Benthos present a
-            compelling blend of progressive metal, math, and jazz-rock — a real universe
-            of sound which you must hear to believe.
+            A curious, experimental, pure rock unit based out of Milan, Benthos
+            present a compelling blend of progressive metal, math, and jazz-rock
+            — a real universe of sound which you must hear to believe.
           </p>
           <br />
           <p>
-            The band performed live on stage for the first time as a support act for
-            <strong>The Contortionist</strong> in early 2019, and suddenly caught the
-            Italian’s scene attention.
+            The band performed live on stage for the first time as a support act
+            for
+            <strong>The Contortionist</strong> in early 2019, and suddenly
+            caught the Italian’s scene attention.
           </p>
           <br />
           <p>
-            In 2020 Benthos were forced to work at home due to Covid-19, but despite the
-            restrictive circumstances, the first album took shape. In 2021, American label
+            In 2020 Benthos were forced to work at home due to Covid-19, but
+            despite the restrictive circumstances, the first album took shape.
+            In 2021, American label
             <a
               href="https://www.eclipserecords.com/"
               target="_blank"
               class="not-italic transition-all duration-400 hover:text-cream"
               >Eclipse Records</a
             >
-            released the band's debut album titled “II”, which impressed the metal and
-            progressive community worldwide.
+            released the band's debut album titled “II”, which impressed the
+            metal and progressive community worldwide.
           </p>
           <br />
           <p>
-            In 2022, Benthos toured around Europe for the “The Essence Tour”, playing in
-            venues across Italy, Germany, and Hungary.
+            In 2022, Benthos toured around Europe for the “The Essence Tour”,
+            playing in venues across Italy, Germany, and Hungary.
+          </p>
+          <br />
+          <p>
+            2023 sees Benthos opening the
+            <a
+              href="https://metalitalia.com/articolo/dissonance-festival-2023-a-giugno-a-milano-con-meshugga-soen-e-altri/"
+              target="_blank"
+              class="not-italic transition-all duration-400 hover:text-cream"
+              >Dissonance Festival 2023</a
+            >
+            in Milan for <strong>Meshuggah</strong> alongside
+            <strong>Soen</strong>, <strong>Destrage</strong>, and many more.
           </p>
           <br />
           <div class="reviews">
             <p class="border-l-2 border-solid border-cream pl-6 italic">
-              “Benthos will have to be measured against this debut album in the future,
-              although they certainly are not afraid of it. These are probably such
-              creative minds who walk around with a note and a pen in order to be able to
-              write down all song ideas immediately”—
+              “Benthos will have to be measured against this debut album in the
+              future, although they certainly are not afraid of it. These are
+              probably such creative minds who walk around with a note and a pen
+              in order to be able to write down all song ideas immediately”—
               <a
                 href="https://time-for-metal.eu/benthos-ii/"
                 target="_blank"
@@ -79,8 +93,8 @@ const galleryUris = [
             </p>
             <br />
             <p class="border-l-2 border-solid border-cream pl-6 italic">
-              "I’ve found myself drawn back again and again to these tracks and am sure
-              many of you will feel the same after you give it a go"—
+              "I’ve found myself drawn back again and again to these tracks and
+              am sure many of you will feel the same after you give it a go"—
               <a
                 href="https://metaltrenches.com/reviews/benthos-ii-album-review-3079"
                 target="_blank"
@@ -91,10 +105,10 @@ const galleryUris = [
             <br />
 
             <p class="border-l-2 border-solid border-cream pl-6 italic">
-              “Benthos set an exclamation point in the international progressive metal
-              scene with II. With a flawless sound, innovative ideas and familiar aspects
-              the Italians have proven that they can also compete on an international
-              level” —
+              “Benthos set an exclamation point in the international progressive
+              metal scene with II. With a flawless sound, innovative ideas and
+              familiar aspects the Italians have proven that they can also
+              compete on an international level” —
               <a
                 href="https://www.morecore.de/review/benthos/ii/"
                 target="_blank"
@@ -104,10 +118,10 @@ const galleryUris = [
             </p>
             <br />
             <p class="border-l-2 border-solid border-cream pl-6 italic">
-              “Exploring the outer fringes of musical convention and coming dangerously
-              close to redefining the concept of progressive metal, a bold new player
-              emerges from the Italian peninsula with a short yet gargantuan stylistic
-              excursion of a debut” —
+              “Exploring the outer fringes of musical convention and coming
+              dangerously close to redefining the concept of progressive metal,
+              a bold new player emerges from the Italian peninsula with a short
+              yet gargantuan stylistic excursion of a debut” —
               <a
                 href="https://www.sonicperspectives.com/album-reviews/benthos-ii/"
                 target="_blank"
@@ -122,7 +136,7 @@ const galleryUris = [
       <div>
         <Section title="Music" class="h-full bg-black text-white">
           <iframe
-            style="border-radius: 12px; display: unset"
+            class="rounded-md drop-shadow-lg"
             src="https://open.spotify.com/embed/playlist/5PPnrtqNsvNpFy0b0UCYbp?utm_source=generator&theme=0"
             :width="isMobile ? '100%' : 400"
             :height="isMobile ? 400 : 635"
