50b1bce5b8756d91819e62e1dd64d914de5b6b5d
Alessandro Tagliani <alessandro.tagliani@tuta.io>
2023-02-01 10:24:36 +0100

    🔎 refactor(screenStore): removed useless code

--- src/stores/useScreen.ts
+++ src/stores/useScreen.ts
@@ -1,16 +1,6 @@
 import { reactive, computed } from "vue";
 import { defineStore } from "pinia";
 
-const screen = reactive({
-  width: window.innerWidth,
-  height: window.innerHeight,
-});
-
-window.addEventListener("resize", () => {
-  screen.width = window.innerWidth;
-  screen.height = window.innerHeight;
-});
-
 export const useScreenStore = defineStore("screen", () => {
   const screen = reactive({
     width: window.innerWidth,
