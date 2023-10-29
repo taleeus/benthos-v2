c5240b12e891c88e27a16c9f2b0727e9e5406eed
Amalia Cicala <cicala.amalia@gmail.com>
2023-10-29 16:23:01 +0100

    update epk

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex justify-between bg-black p-6 text-4xl text-white lg:px-32">
+  <div class="flex items-center justify-between bg-black p-6 text-4xl text-white lg:px-32">
     <a
       href="https://open.spotify.com/artist/0A7v88d7h8uYeGaBhflb45?si=teGeb8D8S92fEpK3VrAxGg"
       target="_blank"
@@ -39,6 +39,12 @@
         class="transition-all duration-400 hover:text-cream"
       ></FontAwesomeIcon
     ></a>
+    <a href="https://www.bandsintown.com/a/1419641-benthos"
+    target="_blank"
+    rel="noopener noreferrer">
+    <svg width="32" height="32" fill="#f0efeb" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><title>Bandsintown icon</title><path d="M18.783 0H24v12h-5.217V0zm-6.261 5h5.217v7h-5.217V5zM6.26 5h5.217v7H6.261V5zM24 24H0V0h5.217v19h13.566v-1H6.26v-5H24v11Z"></path></g></svg>
+
+  </a>
     <a href="mailto:benthos.milano@gmail.com"
       ><FontAwesomeIcon
         icon="envelope"
@@ -46,6 +52,12 @@
       ></FontAwesomeIcon
     ></a>
   </div>
+  
 </template>
 
-<style scoped></style>
+<style scoped>
+svg:hover {
+  fill: #e5cb98;
+  transition: ease-in-out;
+  transition-duration: 400ms;
+}</style>
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -35,11 +35,39 @@ const galleryPlaceholders = [
 <template>
   <div>
     <Section title="Photos" class="bg-white pt-4 lg:pt-6">
-      <GallerySection :gallery-uris="galleryUris" :gallery-placeholders="galleryPlaceholders"></GallerySection>
+      <GallerySection
+        :gallery-uris="galleryUris"
+        :gallery-placeholders="galleryPlaceholders"
+      ></GallerySection>
     </Section>
-    <Section title="Singles" class="bg-debris bg-cover px-0 pb-20 pt-12 text-white">
+    <Section
+      title="Singles"
+      class="bg-debris bg-cover px-0 pb-20 pt-12 text-white"
+    >
       <VideosSection :video-ids="videoIds"></VideosSection>
     </Section>
+    <Section
+      title="Talk to Me, Dragonfly!"
+      class="bg-dragonfly bg-cover bg-center pb-10 text-white"
+    >
+      <div class="pb-4 text-xl uppercase tracking-widest text-white">
+        Live at Dissonance Festival
+      </div>
+      <div class="flex justify-center align-middle">
+        <iframe
+          :width="isMobile ? 480 : isTablet ? 600 : 800"
+          :height="isMobile ? 220 : 450"
+          src="https://www.youtube.com/embed/ue1BDz156Z4"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          rel="0"
+          loading="lazy"
+        ></iframe>
+      </div>
+    </Section>
+
   </div>
 </template>
 
